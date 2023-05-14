fce15457a5396c96cb6a7e16ff675f5e61ad7124
Jauhar Goga <jauhargoga@gmail.com>
2023-05-14 21:09:21 +1200

    First Page Construction
    Added a the menu as a page

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from tkinter import *
 root=Tk()
 root.minsize(height=150, width=150)
 root.title("Party Hire Store Record Keeper")
+
+#Creating the main menu
+def Menu():
+    #Labeling the main menu
+    label_1 = Label(root, text="Main Menu")
