71544cd20f26c6f59b393950ed6238ee37ed7fdb
Goga2070 <jauhargoga@gmail.com>
2023-05-15 13:06:35 +1200

    Menu page construction
    added a menu title and fixed the root.mainloop to run.

--- main.py
+++ main.py
@@ -2,10 +2,20 @@ from tkinter import *
 
 #Creating window for program
 root=Tk()
-root.minsize(height=150, width=150)
+root.minsize(height=300, width=400)
 root.title("Party Hire Store Record Keeper")
 
 #Creating the main menu
 def Menu():
     #Labeling the main menu
     label_1 = Label(root, text="Main Menu")
+    label_1.grid(column=2, row=1)
+
+    #Making buttons to seperate 
+
+
+
+
+
+
+root.mainloop()
