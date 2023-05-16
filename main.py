94dfd9b845fe6d2d80b07e7a0b59e73a367b2ccc
Goga2070 <jauhargoga@gmail.com>
2023-05-16 16:28:27 +1200

    Worked On pages
    Added buttons for going to pages need to add buttons for going back. Added a call for the menu before running file

--- main.py
+++ main.py
@@ -7,15 +7,32 @@ root.title("Party Hire Store Record Keeper")
 
 #Creating the main menu
 def Menu():
+
+        #Making record keeper page
+    def recordpage():
+        root.minsize()
+        #Add a message box for errors
+
+
+    #Making Record Viewing page
+    def viewrecordspage():
+        root.minsize()
+        menubutton_1.destroy()
+        menubutton_2.destroy()
+
+    
     #Labeling the main menu
     label_1 = Label(root, text="Main Menu")
     label_1.grid(column=2, row=1)
 
     #Making buttons to seperate 
+    menubutton_1 = Button(root, text="Add a Record", command=recordpage, activebackground="white")
+    menubutton_1.grid(column=1, row=2)
+    menubutton_2 = Button(root, text="View Records", command=viewrecordspage, activebackground="white")
+    menubutton_2.grid(column=2, row=2)
 
 
 
 
-
-
+Menu()
 root.mainloop()
