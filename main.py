b8cff5a5c1b53b4938b2f7e42966b221c4dcb7ed
Goga2070 <jauhargoga@gmail.com>
2023-05-20 20:09:20 +1200

    COMPLETED THE INPUT PAGE HAHAHHAA
    added a bit of code to make the feather on the window become invisible, did not work so it is currently in comment mode. Completed the page for adding records and not accepting certain values etc...
    
    im going clinically insane

--- main.py
+++ main.py
@@ -1,94 +1,142 @@
 from tkinter import *
 from tkcalendar import *
+import base64, zlib
+import tempfile
+from tkinter import messagebox
 
-#Creating window for program
-root=Tk()
+
+# Making a clear icon instead of feather on program window
+# ICON = zlib.decompress(
+#     base64.b64encode(
+#         "eJxjYGAEQgEBBiDJwZDBy"
+#         "sAgxsDAoAHEQCEGBQaIOAg4sDIgACMUj4JRMApGwQgF/ykEAFXxQRc="
+#     )
+# )
+
+# _, ICON_PATH = tempfile.mkstemp()
+# with open(ICON_PATH, "wb") as icon_file:
+#     icon_file.write(ICON)
+
+# Creating window for program
+root = Tk()
 root.minsize(height=200, width=170)
 root.title("Party Hire Store Record Keeper")
 canvas = Canvas(root)
 
 
-#Making the program look better with rounded corners
+# Making the program look better with rounded corners
 def round_rectangle_border(x1, y1, x2, y2, radius=25, **kwargs):
-        
-    points = [x1+radius, y1,
-              x1+radius, y1,
-              x2-radius, y1,
-              x2-radius, y1,
-              x2, y1,
-              x2, y1+radius,
-              x2, y1+radius,
-              x2, y2-radius,
-              x2, y2-radius,
-              x2, y2,
-              x2-radius, y2,
-              x2-radius, y2,
-              x1+radius, y2,
-              x1+radius, y2,
-              x1, y2,
-              x1, y2-radius,
-              x1, y2-radius,
-              x1, y1+radius, 
-              x1, y1+radius,
-              x1, y1]
+    points = [
+        x1 + radius,
+        y1,
+        x1 + radius,
+        y1,
+        x2 - radius,
+        y1,
+        x2 - radius,
+        y1,
+        x2,
+        y1,
+        x2,
+        y1 + radius,
+        x2,
+        y1 + radius,
+        x2,
+        y2 - radius,
+        x2,
+        y2 - radius,
+        x2,
+        y2,
+        x2 - radius,
+        y2,
+        x2 - radius,
+        y2,
+        x1 + radius,
+        y2,
+        x1 + radius,
+        y2,
+        x1,
+        y2,
+        x1,
+        y2 - radius,
+        x1,
+        y2 - radius,
+        x1,
+        y1 + radius,
+        x1,
+        y1 + radius,
+        x1,
+        y1,
+    ]
 
     return canvas.create_polygon(points, **kwargs, smooth=True)
+
 
 my_rectangle = round_rectangle_border(50, 50, 150, 100, radius=20, fill="blue")
 
 
-#Creating the main menu
+# Creating the main menu
 def Menu():
-
-        #Making record keeper page
+    # Making record keeper page
     def recordpage():
         root.minsize(height=200, width=170)
 
+        # Making everything global to be used on the back function
+        global customer_name_label
+        global item_hired_label
+        global item_quantity_label
+        global reciept_num_label
+        global customer_name_input
+        global item_quantity_input
+        global reciept_num_input
+        global item_hired_input
 
-        #Making everything global to be used on the back function
-        global customernamelabel
-        global itemhiredlabel
-        global itemquantitylabel
-        global recieptnumlabel
-        global customernameinput
-        global itemquantityinput
-        global recieptnuminput
-        global itemhiredinput
+        # Labels for the input boxes
+        customer_name_label = Label(root, text="Customer Name")
+        customer_name_label.grid(column=1, row=2)
+        item_hired_label = Label(root, text="Hired Item")
+        item_hired_label.grid(column=2, row=2)
+        item_quantity_label = Label(root, text="Item Quantity")
+        item_quantity_label.grid(column=3, row=2)
+        reciept_num_label = Label(root, text="reciept Number")
+        reciept_num_label.grid(column=4, row=2)
 
-        #Labels for the input boxes
-        customernamelabel = Label(root,text="Customer Name")
-        customernamelabel.grid(column=1,row=2)
-        itemhiredlabel = Label(root,text="Hired Item")
-        itemhiredlabel.grid(column=2,row=2)
-        itemquantitylabel = Label(root,text="Item Quantity")
-        itemquantitylabel.grid(column=3,row=2)
-        recieptnumlabel = Label(root,text="reciept Number")
-        recieptnumlabel.grid(column=4,row=2)
+        # Input boxes
+        customer_name_input = Entry(root)
+        customer_name_input.grid(column=1, row=3)
+        item_hired_input = Entry(root)
+        item_hired_input.grid(column=2, row=3)
+        item_quantity_input = Entry(root)
+        item_quantity_input.grid(column=3, row=3)
+        reciept_num_input = Entry(root)
+        reciept_num_input.grid(column=4, row=3)
 
-        #Input boxes
-        customernameinput = Entry(root)
-        customernameinput.grid(column=1,row=3)
-        itemhiredinput = Entry(root)
-        itemhiredinput.grid(column=2,row=3)
-        itemquantityinput = Entry(root)
-        itemquantityinput.grid(column=3,row=3)
-        recieptnuminput = Entry(root)
-        recieptnuminput.grid(column=4,row=3)
-
-        #Adding enter button to accept the values enetered 
+        # Adding enter button to accept the values enetered
         def enter():
-            cni = customernameinput.get()
-            ihi = itemhiredinput.get()
-            iqi = itemquantityinput.get()
-            rni = recieptnuminput.get()
-            print(cni,ihi,iqi,rni)
+
+            #Making message boxes for incorrect input values
+
+            #Checks if item quantity inputted is a number
+            try:
+                item_quantity_input_interger = int(item_quantity_input.get())
+            except ValueError:
+                messagebox.showerror("Error", "Please enter number!")
+            
+            #Then checks if item quantity inputted is a number within 500 otherwise becomes invalid
+            if item_quantity_input_interger > 500:
+                messagebox.showerror("Error", "Please enter a number within 500!")
+                item_quantity_input_interger = " "
+
+            cni = customer_name_input.get()
+            ihi = item_hired_input.get()
+            iqi = item_quantity_input_interger
+            rni = reciept_num_input.get()
+            print(cni, ihi, iqi, rni)
 
         enterbutton = Button(root, text="Enter", command=enter)
         enterbutton.grid(column=1, row=5)
-        
-            
 
-        #Destorying previous page elemnts
+        # Destorying previous page elemnts
         menubutton_1.destroy()
         menubutton_2.destroy()
         menubutton_3.destroy()
@@ -96,15 +144,16 @@ def Menu():
         emptylabel1.destroy()
         emptylabel2.destroy()
 
-        #Adding back button for record keeping page
+        # Adding back button for record keeping page
         global button_2
-        button_2= Button(root,text="<--Menu", width="7", command=back, activebackground="red")
-        button_2.grid(column=1,row=1, sticky=W)
+        button_2 = Button(
+            root, text="<--Menu", width="7", command=back, activebackground="red"
+        )
+        button_2.grid(column=1, row=1, sticky=W)
 
-        #Add a message box for errors
+        # Add a message box for errors
 
-
-    #Making Record Viewing page
+    # Making Record Viewing page
     def viewrecordspage():
         root.minsize(height=200, width=170)
         label_1.destroy()
@@ -114,30 +163,30 @@ def Menu():
         emptylabel1.destroy()
         emptylabel2.destroy()
 
-        #Making everything global to be used on the back function
-        global customernamelabel
-        global itemhiredlabel
-        global itemquantitylabel
-        global recieptnumlabel
+        # Making everything global to be used on the back function
+        global customer_name_label
+        global item_hired_label
+        global item_quantity_label
+        global reciept_num_label
 
+        # Labels for the grid boxes
+        customer_name_label = Label(root, text="Customer Name")
+        customer_name_label.grid(column=1, row=2)
+        item_hired_label = Label(root, text="Hired Item")
+        item_hired_label.grid(column=2, row=2)
+        item_quantity_label = Label(root, text="Item Quantity")
+        item_quantity_label.grid(column=3, row=2)
+        reciept_num_label = Label(root, text="reciept Number")
+        reciept_num_label.grid(column=4, row=2)
 
-        #Labels for the grid boxes
-        customernamelabel = Label(root,text="Customer Name")
-        customernamelabel.grid(column=1,row=2)
-        itemhiredlabel = Label(root,text="Hired Item")
-        itemhiredlabel.grid(column=2,row=2)
-        itemquantitylabel = Label(root,text="Item Quantity")
-        itemquantitylabel.grid(column=3,row=2)
-        recieptnumlabel = Label(root,text="reciept Number")
-        recieptnumlabel.grid(column=4,row=2)
-
-        
-        #Adding back button for record viewing page
+        # Adding back button for record viewing page
         global button_2
-        button_2= Button(root,text="<--Menu", width="7", command=back, activebackground="red")
-        button_2.grid(column=1,row=1,sticky=W)
-        
+        button_2 = Button(
+            root, text="<--Menu", width="7", command=back, activebackground="red"
+        )
+        button_2.grid(column=1, row=1, sticky=W)
 
+    # Making a Help Page
     def helppage():
         root.minsize(height=200, width=170)
         label_1.destroy()
@@ -147,56 +196,58 @@ def Menu():
         emptylabel1.destroy()
         emptylabel2.destroy()
 
-        #Adding back button for help page
+        # Making the text for the help page
+
+        # Adding back button for help page
         global button_2
-        button_2= Button(root,text="<--Menu", width="7", command=back, activebackground="red")
-        button_2.grid(column=1,row=1)
+        button_2 = Button(
+            root, text="<--Menu", width="7", command=back, activebackground="red"
+        )
+        button_2.grid(column=1, row=1)
 
+        # Making a back button that can be used on all pages.
 
-
-
-        #Making a back button that can be used on all pages. 
     def back():
         root.minsize(height=200, width=170)
         root.title("Party Hire Store Record Keeper")
         Menu()
 
-        #removing the back button
+        # removing the back button
         button_2.destroy()
 
-        #Removing the other assests,  buttons and labels
-        customernamelabel.destroy()
-        itemhiredlabel.destroy()
-        itemquantitylabel.destroy()
-        recieptnumlabel.destroy()
-        customernameinput.destroy()
-        itemhiredinput.destroy()
-        itemquantityinput.destroy()
-        recieptnuminput.destroy()
-        
-        
+        # Removing the other assests,  buttons and labels
+        customer_name_label.destroy()
+        item_hired_label.destroy()
+        item_quantity_label.destroy()
+        reciept_num_label.destroy()
+        customer_name_input.destroy()
+        item_hired_input.destroy()
+        item_quantity_input.destroy()
+        reciept_num_input.destroy()
 
-
-    #Labeling the main menu
+    # Labeling the main menu
     label_1 = Label(root, text="Main Menu")
     label_1.grid(column=2, row=1)
 
-    #Making buttons to seperate 
-    menubutton_1 = Button(root, text="Add a Record", command=recordpage, activebackground="white")
+    # Making buttons to seperate
+    menubutton_1 = Button(
+        root, text="Add a Record", command=recordpage, activebackground="white"
+    )
     menubutton_1.grid(column=2, row=2)
-    menubutton_2 = Button(root, text="View Records", command=viewrecordspage, activebackground="white")
+    menubutton_2 = Button(
+        root, text="View Records", command=viewrecordspage, activebackground="white"
+    )
     menubutton_2.grid(column=2, row=3)
-    menubutton_3 = Button(root, text="How to Use?", command=helppage, activebackground="white")
+    menubutton_3 = Button(
+        root, text="How to Use?", command=helppage, activebackground="white"
+    )
     menubutton_3.grid(column=2, row=4)
 
-    #Adding a spacing between sides and menu
-    emptylabel1 = Label(root, text="", padx= 20)
-    emptylabel1.grid(column=1,row=2)
-    emptylabel2 = Label(root, text="", padx= 20)
-    emptylabel2.grid(column=3,row=2)
-
-
-
+    # Adding a spacing between sides and menu
+    emptylabel1 = Label(root, text="", padx=20)
+    emptylabel1.grid(column=1, row=2)
+    emptylabel2 = Label(root, text="", padx=20)
+    emptylabel2.grid(column=3, row=2)
 
 
 Menu()
